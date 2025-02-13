7901c938c28619a3fc0833076b5b620c276fd8c2
Albina <yakunina.albina@gmail.com>
2025-02-13 21:18:04 +0100

    Add changes

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,9 +1,19 @@
+const sex = person => {
+  if (person.sex === 'f') {
+    return 'husband';
+  }
+
+  return 'wife';
+};
+
 export const Person = ({ person }) => (
   <section className="Person">
     <h2 className="Person__name">My name is {person.name}</h2>
     {person.age && <p className="Person__age">I am {person.age}</p>}
     {person.isMarried ? (
-      <p className="Person__partner">{person.partnerName} is my wife</p>
+      <p className="Person__partner">
+        {person.partnerName} is my {sex(person)}
+      </p>
     ) : (
       <p className="Person__partner">I am not married</p>
     )}
